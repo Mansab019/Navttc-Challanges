7040c5b0f837c095a729f024bc1bfad138291e66
Mansab019 <mansab.azhar@gmail.com>
2026-06-15 12:13:30 +0500

    Add Task 5 - Calculate Square Root

---
+++ Week_01_Python/task_05_square_root.py
@@ -0,0 +1,9 @@
+# Task 5 - Calculate Square Root
+
+num = 8
+
+# Uncomment below to take input from user
+# num = float(input('Enter a number: '))
+
+num_sqrt = num ** 0.5
+print('The square root of %0.3f is %0.3f' % (num, num_sqrt))

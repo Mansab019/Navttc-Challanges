63add1ade03f0fe1c382b002e7ad92885e57b115
Mansab019 <mansab.azhar@gmail.com>
2026-06-15 12:14:19 +0500

    Add Task 9 - Solve Quadratic Equation

---
+++ Week_01_Python/task_09_quadratic_equation.py
@@ -0,0 +1,16 @@
+# Task 9 - Solve Quadratic Equation ax^2 + bx + c = 0
+
+import cmath
+
+a = 1
+b = 5
+c = 6
+
+# Calculate the discriminant
+d = (b ** 2) - (4 * a * c)
+
+# Find two solutions
+sol1 = (-b - cmath.sqrt(d)) / (2 * a)
+sol2 = (-b + cmath.sqrt(d)) / (2 * a)
+
+print('The solutions are {0} and {1}'.format(sol1, sol2))
